f52dd7e3f7ae1a5ee9262152dcbed25f1eafd7a8
Sejin Kim <54730100+kim3-sudo@users.noreply.github.com>
2019-10-02 11:45:18 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+var searchType;  // search type variable
+
+$(document).ready(function () {
+    console.log("start!");
+    searchType="Last";
+    $("#search-btn").click(getMatches);
+
+    $("#clear").click(clearResults);
+
+    $(".dropdown-menu li a").click(function(){
+	console.log("pick!"+$(this).text());
+	$(this).parents(".btn-group").find('.selection').text($(this).text());
+	searchType=$(this).text();
+    });
+});
+
+// Build output table from comma delimited list
+function nameTable(list) {
+    var result = '<table class="w3-table-all w3-hoverable" border="2"><tr><th>Name</th><th>%</th><th>Rank</th><tr>';
+    var a = list.split(",");
+    var aLen = a.length;
+    for (var i = 0; i < aLen; i+=3) {
+	result += "<tr><td>"+a[i]+"</td><td>"+a[i+1]+"</td><td>"+a[i+2]+"</td></tr>";
+    }
+    result += "</table>";
+
+    return result;
+}
+
+
+function processResults(results) {
+    $('#searchresults').empty();
+    $('#searchresults').append(nameTable(results));
+}
+
+function clearResults() {
+    $('#searchresults').empty();
+}
+
+function getMatches(){
+    if ($('#search').val().length < 2) return;
+    $('#searchresults').empty();
+    $.ajax({
+		url: '/cgi-bin/kim3_namelookup.cgi?name='+$('#search').val()+'&type_select='+searchType,
+		dataType: 'text',
+		success: processResults,
+		error: function(){alert("Error: Something went wrong");}
+    });
+
+}
