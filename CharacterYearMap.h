202947ad7c35d2ca9b510e2a575072f13ff8b0de
Sejin Kim <54730100+kim3-sudo@users.noreply.github.com>
2019-10-07 01:12:42 -0400

    Update CharacterYearMap.h

--- CharacterYearMap.h
+++ CharacterYearMap.h
@@ -2,16 +2,7 @@
 CharacterYearMap.h - Object to store a map of all years 
 and the lines in the data file that name appears on.
 ******************************************************/
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <map>
-#include <algorithm>
-#include <vector>
-#include <cstring>
-#include <string.h>
-#include <string>
-#include <stdio.h>
+#include "includes.h"
 #include "CharacterProfile.h"
 
 using namespace std;
